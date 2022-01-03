2f18aec14aa7c72dbc509f616b1533b4945d4095
ariful2020 <programmerariful@gmail.com>
2022-01-04 04:58:37 +0600

    Upload file Assignment 12

---
+++ js/app.js
@@ -0,0 +1,107 @@
+// =============== Age Calculation =====================
+
+const name = document.querySelector('#name');
+const year = document.querySelector('#year');
+const sub_form = document.querySelector('#sub_form');
+const result = document.querySelector('.result');
+
+sub_form.addEventListener ('submit', (e) => {
+    e.preventDefault();
+    let Name = name.value;
+    let Year = year.value;
+    let nPattern = /^[a-zA-Z ]*$/;
+    let yPattern = /^[0-9]{4,4}$/;
+
+if(Name == '' || Year == ''){
+    result.innerHTML = `<div class="alert alert-success text-center" >All Field Required !</div>`
+
+    name.value = "";
+    year.value = "";
+}else if (nPattern.test(Name) == false || yPattern.test(Year) == false ){
+    result.innerHTML = `<p class= "alert alert-warning">Names must be letters and years must be numbers.</p>`;
+    name.value = "";
+    year.value = "";
+}else{
+    result.innerHTML = ageCal (Name, Year);
+    name.value = "";
+    year.value = "";
+} 
+})
+
+// ================= Currency Converter ==================
+
+const con_form = document.querySelector('#Currency_con-form');
+const amount_here = document.querySelector('#Amount_here');
+const cor_select_from = document.querySelector('#cor_select');
+const cor_select_to = document.querySelector('#cor_select_2');
+const cal_result = document.querySelector('#cal_result');
+const fa_dollar_sign = document.querySelector('.fa-dollar-sign')
+const fa_pound_sign = document.querySelector('.fa-pound-sign')
+const fa_euro_sign = document.querySelector('.fa-euro-sign')
+
+
+
+con_form.onsubmit = (e) => {
+    e.preventDefault()
+    let amount = amount_here.value;
+    let from_value = cor_select.value;
+    let to_value = cor_select_to.value;
+    let first_con_result = amount * from_value;
+    let con_result = first_con_result / to_value
+    
+
+    if (amount == '' || from_value == 0 || to_value == 0){
+        cal_result.innerHTML = `<span class=" text-success"> All Field Required! </span>` 
+    }else if (to_value == 1){
+        cal_result.innerHTML = `<p class="text-success">Congratulations, Your Total Amount ${con_result.toFixed(2)} BDT</p>` 
+    }else if (to_value == 85){
+        cal_result.innerHTML = `<p class="text-success">Congratulations, Your Total Amount ${con_result.toFixed(2)} DOLLAR</p>` 
+    }else if (to_value == 67){
+        cal_result.innerHTML = `<p class="text-success">Congratulations, Your Total Amount ${con_result.toFixed(2)} DOLLAR</p>` 
+    }else if (to_value == 100){
+        cal_result.innerHTML = `<p class="text-success">Congratulations, Your Total Amount ${con_result.toFixed(2)} EURO</p>` 
+    }else if (to_value == 115){
+        cal_result.innerHTML = `<p class="text-success">Congratulations, Your Total Amount ${con_result.toFixed(2)} Pound</p>` 
+    }else{
+        cal_result.innerHTML = `<p class="text-warning">WARNING! Wrong Number</P>`
+    }
+    cal_result.style.border="1px solid #198754"
+    amount_here.value = "";
+    cor_select.value = 0;
+    cor_select_to.value = 0;
+}
+
+// ================= Marriage Calculation ============= 
+
+const marriage_cal = document.querySelector('#marriage_cal');
+const first_name = document.querySelector('.first_name');
+const last_name = document.querySelector('.last_name');
+const marriage_year = document.querySelector('#marriage_year');
+const gender = document.querySelector('#gender');
+const m_check_result = document.querySelector('#m_check_result');
+
+marriage_cal.onsubmit = (e) => {
+    e.preventDefault();
+let f_name = first_name.value;
+let l_name = last_name.value;
+let name = f_name + ' ' + l_name;
+let m_year = marriage_year.value;
+let m_gender = gender.value;
+let m_n_Pattern = /^[a-zA-Z ]*$/;
+let m_y_Pattern = /^[0-9]{4,4}$/;
+
+if (f_name == '' || l_name == '' || m_year == '' || m_gender == 0){
+    m_check_result.innerHTML = `<h3 class= "alert alert-warning text-center">Warning! All Field Required</h3>`
+}else if (m_n_Pattern.test(f_name) == false || m_n_Pattern.test(l_name) == false || m_y_Pattern.test(m_year) == false){
+    m_check_result.innerHTML = `<h5 class= "alert alert-warning text-center">Name must be letter and year must be number</h5>`
+}else{
+    m_check_result.innerHTML = marriage_check(name, m_year, m_gender);
+}
+
+first_name.value = '';
+last_name.value = '';
+marriage_year.value = '';
+gender.value = 0;
+
+
+}
---
+++ js/function.js
@@ -0,0 +1,128 @@
+/**
+ * Age Calculator
+ * @param {*} name 
+ * @param {*} age 
+ * @returns Age
+ */
+function ageCal (name, age){
+    let date = new Date();
+    let ageCalRes = date.getFullYear() - age;
+    let ageComment = ageStatus(ageCalRes)
+    return `
+    <div class = "alert alert-${ageComment.status}">
+    Hello ${name}, Tomar Boyos ${ageCalRes} Bosor & Tumi ${ageComment.comment}
+    </div> `
+};
+
+function ageStatus(Age){
+
+ if (Age > 0 && Age < 10 ){
+     return {
+         comment : 'Babu',
+         status : 'primary'
+     }
+
+}else if(Age >= 10 && Age < 18 ){
+return {
+    comment : 'Kishor',
+    status : 'secondary'}
+
+}else if(Age >= 18 && Age < 36 ){
+return {
+    comment : 'Jubok',
+    status : 'success'} 
+
+}else if(Age >= 36 && Age < 50 ){
+return {
+    comment : 'Boyosko',
+    status : 'info'}
+        
+}else if(Age >= 50 && Age < 70 ){
+return {
+    comment : 'Briddho',
+    status : 'warning'}
+            
+}else{
+return {
+    comment : 'Jin',
+    status : 'danger'}
+                
+}
+}
+
+// =========== Marriage Checker ==============
+
+
+/**
+ * Marriage Checker
+ * @param {*} name 
+ * @param {*} m_age 
+ * @returns age status
+ */
+function marriage_check(name, m_age, m_gender){
+    let m_date = new Date();
+    let marriage_age = m_date.getFullYear() - m_age;
+    let congra = "Congratulations.";
+    let p_status = "Tomar Biyer Boyos Hoyeche. Tomar Jonne Sundor Patri Royeche";
+    let sorry = "Sorry!";
+    let n_status = "Tomar Biyer Boyos Hoini.";
+    let kom_ase = 18 - marriage_age;
+    let permission = "Tomar Boyos Bashi. Miyer Thake Onumoti Nite Hobe.";
+    let patri = "Tomar Jonne Kono Patri Nei";
+    let name_con = con_sorry_name(m_gender);
+
+if (marriage_age > 0 && marriage_age < 17){ 
+
+    return `
+    <div class="out_put">
+        <h1 class="congra text-center text-warning">${sorry} ${name_con}.</h1>
+        <h2>Name  <span class="span-1">: ${name}</span></h2>
+        <h4>Age  <span class="span-2">: ${marriage_age} Years.</h4>
+        <h4>Gender <span class="span-3">: ${m_gender}</span></h4>
+        <h4>Status <span class="span-4">:</span><span class="status"> ${n_status}. Tumi ${kom_ase} Year Por Biye Korte Parbe</span></h4>
+    </div>
+ `
+}else if (marriage_age >= 18 && marriage_age <= 35){ 
+    return `
+    <div class="out_put">
+        <h1 class="congra text-center text-success">${congra} ${name_con}.</h1>
+        <h2>Name  <span class="span-1">:</span> ${name}</h2>
+        <h4>Age  <span class="span-2">:</span> ${marriage_age} Years</h4>
+        <h4>Gender <span class="span-3">:</span> ${m_gender}</h4>
+        <h4>Status <span class="span-4">:</span><span class="status">${p_status}</span></h4>
+    </div>
+ `
+}else if (marriage_age > 35 && marriage_age < 55){ 
+    return `
+    <div class="out_put">
+        <h1 class="congra text-center text-warning">${sorry} ${name_con}.</h1>
+        <h2>Name  <span class="span-1">:</span> ${name}</h2>
+        <h4>Age  <span class="span-2">:</span> ${marriage_age} Years</h4>
+        <h4>Gender <span class="span-3">:</span> ${m_gender}</h4>
+        <h4>Status <span class="span-4">:</span><span class="status"> ${permission}</span></h4>
+    </div>
+ `
+}else{
+    return`<div class="out_put">
+    <h1 class="congra text-center text-warning">${sorry} ${name_con}.</h1>
+    <h2>Name  <span class="span-1">:</span> ${name} </h2>
+    <h4>Age  <span class="span-2">:</span> ${marriage_age} Years</h4>
+    <h4>Gender <span class="span-3">:</span> ${m_gender}</h4>
+    <h4>Status <span class="span-4">:</span><span class="status">${patri}</span></h4>
+</div>
+`
+}}
+
+function con_sorry_name(gender){
+
+    if(gender == 'Male'){
+        return `Sir`
+    }else if(gender = 'Fenmale'){
+        return `Madam`
+    }else{
+        return`Wrong`
+    }
+
+
+
+}
